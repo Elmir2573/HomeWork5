dbda9f3a578ab712e4507d11f60a283815eae136
zzr-i@mail.ru <zzr-i@mail.ru>
2024-12-06 12:28:18 +0300

    HW 5 на вторую повторную проверку

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача № 1");
-        int clientOS = 3;
+        int clientOS = 2;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 1)
             System.out.println("Установите версию приложения для Android по ссылке");
-        else {
+        else if (clientOS > 1) {
             System.out.println("Уточните, какая ОС на вашем смартфоне?");
         }
         System.out.println("Задача № 2");
@@ -24,14 +24,14 @@ public class Main {
         }
         System.out.println("Задача № 3");
         int year = 2021;
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year > 1854)) {
+        if ((year > 1854 && year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
             System.out.println(+year + " год является високосным");
+        } else if (year < 1854) {
+            System.out.println("Год не может быть меньше 1584");
         } else {
             System.out.println(+year + " год не является високосным");
         }
-        if (year < 1854) {
-            System.out.println("Год не может быть меньше 1584");
-        }
+
         System.out.println("Задача № 4");
         int deliveryDistance = 95;
         int deliveryTime = 1;
@@ -49,24 +49,16 @@ public class Main {
         System.out.println("Задача № 5");
         int monthNumber = 12;
         switch (monthNumber) {
-            case 1:
-            case 2:
-            case 12:
+            case 1, 2, 12:
                 System.out.println("Месяц принадлежит к сезону зима");
                 break;
-            case 3:
-            case 4:
-            case 5:
+            case 3, 4, 5:
                 System.out.println("Месяц принадлежит к сезону весна");
                 break;
-            case 6:
-            case 7:
-            case 8:
+            case 6, 7, 8:
                 System.out.println("Месяц принадлежит к сезону лето");
                 break;
-            case 9:
-            case 10:
-            case 11:
+            case 9, 10, 11:
                 System.out.println("Месяц принадлежит к сезону осень");
                 break;
             default:
